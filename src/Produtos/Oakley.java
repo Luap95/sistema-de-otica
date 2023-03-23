a581e392a9e1c3ebee7878f188be23575a2ff3d1
Lucas <lucas.7795.la@gmail.com>
2023-03-23 20:21:02 -0300

    Criação da classe Oakley

---
+++ src/Produtos/Oakley.java
@@ -0,0 +1,10 @@
+package Produtos;
+
+public class Oakley extends Armacao{
+    public Oakley(){
+        super.setNome("Oakley esportiva");
+        super.setPreco(580.90);
+        super.setGrife("Oakley");
+        super.setCor("Preta");
+    }
+}
