d32a12e8dca3635638f3b97890b2b913ea853c9f
Lucas <lucas.7795.la@gmail.com>
2023-03-23 20:14:56 -0300

    Criação da classe Armação

--- src/Produtos/Armacao.java
+++ src/Produtos/Armacao.java
@@ -1,4 +1,20 @@
 package Produtos;
 
-public class Armacao {
+public class Armacao extends Produto {
+    private String grife;
+    private String cor;
+
+    public void setGrife(String grife){
+        this.grife = grife;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public String getGrife(){
+        return this.grife;
+    }
+    public String getCor(){
+        return this.cor;
+    }
 }
